a314266cf7be8c1b0174e3c852ca3bffba977729
Meriya Susan <contactmeriyaforwork@gmail.com>
2024-11-04 21:20:37 -0500

    reverted changes to seeding

--- seeding/kijiji/kijiji_seed.py
+++ seeding/kijiji/kijiji_seed.py
@@ -2,8 +2,8 @@ import pandas as pd
 from pymongo import MongoClient
 
 # Load the scraped data from CSV
-data = pd.read_csv('scrape/kijiji/ontario/ontario_listings.csv')
-    # '/Users/ernestgaisie/Desktop/Housing Prices Project/scrape/kijiji/ontario/ontario_listings.csv')
+data = pd.read_csv(
+    '/Users/ernestgaisie/Desktop/Housing Prices Project/scrape/kijiji/ontario/ontario_listings.csv')
 
 # Clean the Price column: Remove '$' and ',' then convert to numeric, handling non-numeric entries as NaN
 data['Price'] = data['Price'].replace('[\$,]', '', regex=True)  # Remove symbols
@@ -13,7 +13,7 @@ data['Price'] = pd.to_numeric(data['Price'], errors='coerce')  # Convert to floa
 data.dropna(subset=['Price'], inplace=True)
 
 # Connect to MongoDB
-client = MongoClient('mongodb+srv://meriyageorgebethlem:Mongo2024@cluster0.6mgbbnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+client = MongoClient('mongodb+srv://ernestyawgaisie:ernestyawgaisie@cluster0.dvjsafm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db = client['real_estate']  # Use database named 'real_estate'
 collection = db['raw_data_with_price_cleaned']  # Collection for price-cleaned data
 
@@ -23,4 +23,3 @@ data_dict = data.to_dict("records")
 # Insert cleaned data into MongoDB
 collection.insert_many(data_dict)
 print("Data with cleaned 'Price' has been successfully stored in MongoDB.")
-
